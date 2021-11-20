8170965d471b0e777db74628c62f1ce3a2c9fa69
Aziz <azizekeathn@gmail.com>
2021-11-20 23:44:19 +0100

    day6twee

---
+++ ornek10_useEffect/src/UseEffect01.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//! - useEffect Hook'u side effect(yan etki)'leri yonetmek icin kullanilan bir Hook'tur.
+
+//!- Class komponentlerindeki componentDidMount(), componentDidUnMount(),
+//!  componentDidUpdate() gibi yaşamdöngüsü (lifecycle) metodlarinin
+//!  yaptigi isi yapar.
+
+//! - Bu ozellikleri sayesinde genelde uygulama dis dunyadan (veritabani, api)
+//!   veri cekerken kullanimi yaygindir.
+
+function UseEffect01() {
+  return <div></div>;
+}
+
+export default UseEffect01;
---
+++ ornek10_useEffect/src/index.js
@@ -0,0 +1,13 @@
+//Reactin temel kutuphanleri
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(
+  <div>
+    <App /> {/* App modulunun cagrilmasi.*/}
+  </div>,
+
+  document.getElementById("root")
+);
