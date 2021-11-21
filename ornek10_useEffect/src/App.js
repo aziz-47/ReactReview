cbf7e65fb9b4c3e4b30f7657f421d3567afbee11
Aziz <azizekeathn@gmail.com>
2021-11-21 14:04:51 +0100

    day6

--- ornek10_useEffect/src/App.js
+++ ornek10_useEffect/src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <div className="App"></div>;
-}
+import UseEffect01 from "./UseEffect01";
 
+function App() {
+  return (
+    <div className="App">
+      <UseEffect01 />
+    </div>
+  );
+}
 export default App;
