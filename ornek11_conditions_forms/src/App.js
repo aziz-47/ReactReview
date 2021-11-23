75f093be3158a9eb144e7c9554d50c5f01057365
Aziz <azizekeathn@gmail.com>
2021-11-23 14:33:56 +0100

    commit

---
+++ ornek11_conditions_forms/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO REACT</h1>
+      <p>React UI gelistirmek icin kullanilan bir javascript kutuphanesidr.</p>
+    </div>
+  );
+}
+
+export default App;
